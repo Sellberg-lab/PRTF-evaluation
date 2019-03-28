6b36adfdc856fa915261e85261a94fb1c4bcc8f0
Vidar Elsin <elsin@kth.se>
2019-03-28 11:07:08 +0100

    Script to visualize phase of test particles, true or reconstructed

---
+++ plot_phase_diagram.py
@@ -0,0 +1,28 @@
+# Script to plot a phase diagram of particle, centered correctly
+
+import numpy as np
+import h5py as h5
+import matplotlib.pyplot as plt
+import sys
+
+filename = str(sys.argv[1])
+shift = False
+if sys.argv[2]:
+    if sys.argv[2] == 'shift':
+        shift = True
+
+f = h5.File(filename, 'r')
+
+# retrieve from h5 and remove useless 3rd dimension
+imag = f['imag'][:].reshape((1024,1024))
+real = f['real'][:].reshape((1024,1024))
+
+# shifting
+if shift:
+    imag = np.fft.fftshift(imag)
+    real = np.fft.fftshift(real)
+
+phase = np.angle(real + imag*1j)
+
+plt.imshow(phase)
+plt.show()
