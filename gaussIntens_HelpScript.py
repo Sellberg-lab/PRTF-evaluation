3c7897de4f7cbf83aeaa4bb37fcf29d9281625b1
Alfred-N <47230525+Alfred-N@users.noreply.github.com>
2019-04-25 00:08:44 +0200

    Add files via upload

--- gaussIntens_HelpScript.py
+++ gaussIntens_HelpScript.py
@@ -10,6 +10,6 @@ from gaussian_intensity import gaussian_intensity
 particleList= ["particle_001","particle_002","particle_003","particle_004","particle_005","particle_006"]
 std=0.1
 for particle in particleList:
-    full_filename="/home/alfredn/scratch/test_particles/"+particle+"/particle_detected_intensity.h5" 
-    full_savefile="/home/alfredn/scratch/Gaussian_test_particles/"+particle+"/particle_detected_intensity.h5"
+    full_filename="/home/elsin/scratch/test_particles/"+particle+"/particle_detected_intensity.h5" 
+    full_savefile="/home/alfredn/scratch/Gaussian_test_particlesv2/"+particle+"/particle_detected_intensity.h5"
     gaussian_intensity(full_filename,full_savefile,std)
