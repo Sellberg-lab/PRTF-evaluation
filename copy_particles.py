148c3b48f58f8b4dca49083dcce54b93ef7f0aa0
Alfred-N <47230525+Alfred-N@users.noreply.github.com>
2019-04-25 00:04:17 +0200

    Add files via upload

--- copy_particles.py
+++ copy_particles.py
@@ -10,9 +10,10 @@ import os
 particleList= ["particle_001","particle_002","particle_003","particle_004","particle_005","particle_006"]
 
 for i,particle in enumerate(particleList):
-    command1="cp ../elsin/scratch/test_particles/"+particle+"/particle_diffracted_wave.h5"
-    command2="cp ../elsin/scratch/test_particles/"+particle+"/particle_shape.hdf5"
-    command3="cp ../elsin/scratch/test_particles/"+particle+"/particle_support.h5"
+
+    command1="cp /home/elsin/scratch/test_particles/"+particle+"/particle_diffracted_wave.h5"
+    command2="cp /home/elsin/scratch/test_particles/"+particle+"/particle_shape.hdf5"
+    command3="cp /home/elsin/scratch/test_particles/"+particle+"/particle_support.h5"
     dest = " /home/alfredn/scratch/Gaussian_test_particlesv2/"+particle
     
     os.system(command1+dest)
