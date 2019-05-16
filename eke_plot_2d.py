ed095022701d9368d406acb70788ece277b44cca
Vidar Elsin <elsin@kth.se>
2019-05-16 10:43:30 +0200

    prtf plotting stuff

---
+++ eke_plot_2d.py
@@ -0,0 +1,4 @@
+#!/davinci/Cellar/Python/miniconda3/envs/py2/bin/python
+# EASY-INSTALL-SCRIPT: 'python-tools==1.0','eke_plot_2d.py'
+__requires__ = 'python-tools==1.0'
+__import__('pkg_resources').run_script('python-tools==1.0', 'eke_plot_2d.py')
